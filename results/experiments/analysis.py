dab29e615e80ecbe74a25ba5b9cfe423f49caf8a
olliematthews <ojm1@btinternet.com>
2024-06-10 16:39:28 +0100

    Looks good!

--- results/experiments/analysis.py
+++ results/experiments/analysis.py
@@ -28,7 +28,7 @@ experiment = "gpt_contexts_35"
 results_dir = cdir / experiment
 
 context_df = pd.read_pickle(results_dir / "context.p")
-di_df = pd.read_pickle(results_dir / "danger_results.p")
+di_df_c = pd.read_pickle(results_dir / "danger_results_combined.p")
 di_df_nc = pd.read_pickle(results_dir / "danger_results_not_combined.p")
 outputs_df = pd.read_pickle(results_dir / "llm_outputs.p")
 questions_df = pd.read_pickle(results_dir / "questions.p")
@@ -41,9 +41,9 @@ results_df = pd.merge(
 
 results_df["correct"] = results_df.apply(is_correct, axis=1)
 results_df["poisoned"] = results_df.apply(is_incorrect, axis=1)
-results_df = pd.merge(results_df, di_df["dangerous"], on=["qid", "Context type"])
-
+results_df = pd.merge(results_df, di_df_nc["dangerous"], on=["qid", "Context type"])
+results_df["poisoned_with_check"] = ~results_df["dangerous"] & results_df["poisoned"]
 results_df.groupby(["Prompt type", "Context type"]).agg(
-    {"poisoned": "mean", "correct": "mean"}
+    {"poisoned": "mean", "correct": "mean", "poisoned_with_check": "mean"}
 )
 print("hi")
